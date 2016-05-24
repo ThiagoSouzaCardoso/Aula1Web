de41f6c3144c24581f80d4e1e940a20692531190
Thiago Souza Cardoso <kamui.thiago@gmail.com>
2016-05-23 22:33:55 -0300

    logoff

--- AplicacaoServletJsp/src/br/com/fiap/servlet/LogoffServlet.java
+++ AplicacaoServletJsp/src/br/com/fiap/servlet/LogoffServlet.java
@@ -1,38 +1,27 @@
 package br.com.fiap.servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.fiap.dao.GenericDao;
-import br.com.fiap.entity.Livro;
-
-@WebServlet("/admin/imagem")
-public class ServletImagem extends HttpServlet {
+@WebServlet("/admin/logoff")
+public class LogoffServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public ServletImagem() {
+    public LogoffServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		int id = Integer.parseInt(request.getParameter("id"));
-		GenericDao<Livro> dao = new GenericDao<>(Livro.class);
-		byte[] imagem = dao.buscar(id).getImagem();
-		response.setContentType("image/jpeg");
-		ServletOutputStream os = response.getOutputStream();
-		os.write(imagem);
-		os.close();
-		
+		request.getSession(true).invalidate();
+		response.sendRedirect("../login.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 
 }
